e22adb4b717f6cc5bd35cc799de0983ab628dfeb
Summerly <Summerly.Xi@gmail.com>
2018-11-21 15:40:36 +0800

    Use timestampable traits

--- src/Entity/Record.php
+++ src/Entity/Record.php
@@ -3,12 +3,19 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\RecordRepository")
  */
 class Record
 {
+    /**
+     * Hook timestampable behavior
+     * updates createdAt, updatedAt fields
+     */
+    use TimestampableEntity;
+
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
